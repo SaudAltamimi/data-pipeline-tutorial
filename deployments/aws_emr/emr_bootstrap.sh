57398838482df47dc77218c38dcb67ad062b76fc
S3od24 <saud.altammimi@gmail.com>
2020-06-27 19:29:19 +0300

    implement cleanse_categories function

---
+++ deployments/aws_emr/emr_bootstrap.sh
@@ -0,0 +1,7 @@
+#!/bin/bash
+sudo easy_install pip3
+sudo pip3 install -U \
+    matplotlib \
+    pandas \
+    koalas \
+    pyarrow
--- src/etl.py
+++ src/etl.py
@@ -1,5 +1,27 @@
 from pyspark.sql import SparkSession
 import databricks.koalas as ks
+import pandas as pd
+
+
+# helper function
+def _split_and_expand(categories_text:str, by:str = ';'):
+    '''
+    A function that mimics pandas.str.split(expand=True) behavior, 
+    to be applied against koalas DataFrame
+    '''
+    # split --> to get list of categories with their boolean values
+    categories_text_splitted = categories_text.split(';')
+    
+    # get the categories boolean values only 
+    cat_booleans = [cat.split('-')[1] 
+                    for cat in categories_text_splitted
+                   ]
+    
+    
+    # expand the splitted list to multiple of columns
+    cat_booleans = pd.Series(cat_booleans).astype(int)
+    
+    return cat_booleans
 
 
 def extract_data():
@@ -13,8 +35,36 @@ def extract_data():
 
 
 def cleanse_categories(kdf:'DataFrame'):
-    ''' TODO '''
-    return kdf
+    ''' 
+    this function will do two things to cleanse categories:
+    1- extract the categoreis for one row and assign them as columns
+    2- extract the boolean values for each category 
+    '''
+    # extract columns names --> categories names
+    cols = (kdf
+            .categories.str.split(';')
+            .apply(lambda cats_list: [cat.split('-')[0] 
+                                      for cat in cats_list]
+                                      )
+            ).loc[0]
+    
+    
+    # set 'id' as index to not lose it
+    kdf = kdf.set_index('id')
+    
+    
+    # extract the boolean values for each category
+    kdf = (kdf
+           .apply(lambda row: _split_and_expand(row.categories),
+                  axis=1
+                  )
+          )
+    
+    
+    # assign the new column names
+    kdf.columns = cols
+
+    return kdf.reset_index()
 
 
 def merge_data(kdf:'DataFrame', other_data:'DataFrame', on:str):
@@ -26,9 +76,14 @@ def validate_data(kdf:'DataFrame'):
     return kdf
     
 
-def load_data(kdf:'DataFrame', where:str):
+def load_data(kdf:'DataFrame', where:str, partition_by:list):
     ''' save the prepared data '''
-    kdf.to_csv(where)
+    (kdf
+    .to_spark()
+    .write
+    .partitionBy(*partition_by)
+    .parquet(where)
+    )
 
 
 
@@ -37,6 +92,7 @@ def main():
     # Create a sspark session
     spark_session = (SparkSession
                     .builder
+                    .appName('data_pipeline_example')
                     .getOrCreate()
                     )
 
@@ -46,12 +102,19 @@ def main():
     # main etl pipeline
     (kdf_categories
     .pipe(cleanse_categories)
-    .pipe(merge_data, other_data = kdf_messages, on = 'id')
+    .pipe(merge_data,
+          other_data = kdf_messages,
+          on = 'id'
+          )
     .pipe(validate_data)
     .pipe(load_data,
-          where = '../data/cleansed_data.csv'
+          where = '../data/cleansed_data.csv',
+          partition_by = ['genre']
          )
     )
+
+    # stop the spark application
+    spark_session.stop()
 
 
 if __name__=='__main__':
